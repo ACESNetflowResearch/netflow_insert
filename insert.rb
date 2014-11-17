90df8f51a62245d1d29575d1d85b13b75669e330
ashton <ashton.webster@gmail.com>
2014-11-17 14:14:43 -0500

    working with files

--- insert.rb
+++ insert.rb
@@ -9,39 +9,37 @@ begin
     con = Mysql.new 'localhost', 'root', 'cyberaces', 'netflow_db'
     line_re = Regexp.new('(\d{4}\-\d{1,2}\-\d{1,2})\s(\d{1,2}\:\d{1,2}\:\d{1,2}\.\d{1,3})\s+(\d{1,3}\.\d{1,3})\s(\w+)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\:(\d+)\s+\W+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\:(\d+)\s+(\.+[A-Z]*\.*[A-Z]*)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)')
 
-    is_first = true;
-    count = 0;
-    values = "";
+	   is_first = true;
+	   count = 0;
+	   values = "";
 
-    f.each_line do |line| 
-	   if line =~ line_re then
-		  #handle if there should be a comma between values or not
-		  if !is_first then values.concat ", " else is_first = false end
-		  values.concat "(\'#{$1} #{$2}\', 
-		  #{$3}, 
+	   f.each_slice(1000) do |lines| 
+		  lines.each do |line|
+			 if line =~ line_re then
+				#handle if there should be a comma between values or not
+				if !is_first then values.concat ", " else is_first = false end
+				values.concat "(\'#{$1} #{$2}\', 
+				#{$3}, 
 		    \"#{$4}\", 
 		    INET_ATON(\"#{$5}\"), 
-		  #{$6}, 
+				#{$6}, 
 		    INET_ATON(\"#{$7}\"), 
-		  #{$8}, 
+				#{$8}, 
 		    \"#{$9}\", 
-		  #{$10}, #{$11}, #{$12}, #{$13}, #{$14}, #{$15}, #{$16} ) "
-		  
-		  count = count + 1;
-		  #every 101 records, write to database
-		  if (count % 10000) == 0 then
-			 puts "inserted line number #{count} to database"
-			 query =  "INSERT INTO `netflow_full_2` ( `date`, `duration`, `protocol`, `src_ip`, `src_port`, `dest_ip`, `dest_port`, `flags`, `tos`, `packets`, `bytes`, `pps`, `bps`, `bpp`, `flows`) VALUES " + values + ";" 
-			 #puts query;
-			 con.query(query);
-			 #reset values 
-			 values = "";
-			 is_first = true;
+				#{$10}, #{$11}, #{$12}, #{$13}, #{$14}, #{$15}, #{$16} ) "
+			 end
 		  end
 
+		  count = count + 1;
+		  #every 101 records, write to database
+		  puts "inserted line number #{count * 1000} to database"
+		  query =  "INSERT INTO `netflow_full_2` ( `date`, `duration`, `protocol`, `src_ip`, `src_port`, `dest_ip`, `dest_port`, `flags`, `tos`, `packets`, `bytes`, `pps`, `bps`, `bpp`, `flows`) VALUES " + values + ";" 
+		  #puts query;
+		  con.query(query);
+		  #reset values 
+		  values = "";
+		  is_first = true;
 	   end
-
-    end
 rescue Mysql::Error => e
     puts e.errno
     puts e.error
